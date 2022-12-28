0fa8f9fbd07a2e1937fcc6c979db51588d3e516e
macanolli <39697706+macanolli@users.noreply.github.com>
2022-12-28 18:53:54 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd
+echo "this may take a bit..."
+# Updating System
+# dnf update -y
+if [[ "${UPDATE}" == "y" ]]; then
+  debug "Updating system..."
+  debug "$(sudo dnf update -y 2>&1)"
+fi
 
 cp -r /home/dotconfig ~/.config
 cp /home/bashrc ~/.bashrc
